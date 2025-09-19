d0e98570470620e9d6aaeae622ecc4d570512971
egoing <egoing@gmail.com>
2025-09-19 15:10:01 +0900

    글읽기구현

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -5,5 +5,12 @@ type ReadProps = {
 };
 export default async function Read(props: ReadProps) {
     const params = await props.params;
-    return <div>Read, {params.id}</div>;
+    const resp = await fetch(`http://localhost:9999/topics/${params.id}`);
+    const topic = await resp.json();
+    return (
+        <div>
+            <h2>{topic.title}</h2>
+            {topic.body}
+        </div>
+    );
 }
