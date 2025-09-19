212e3be11a5c498a5f1a791843c6b5cbc4dae70d
egoing <egoing@gmail.com>
2025-09-19 11:25:58 +0900

    기본구조

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
 export default function Home() {
-    return <div>hi</div>;
+    return (
+        <div>
+            <h1>
+                <a href="/">Web</a>
+            </h1>
+            <ul>
+                <li>
+                    <a href="/read/1">html</a>
+                </li>
+                <li>
+                    <a href="/read/2">css</a>
+                </li>
+                <li>
+                    <a href="/read/3">javascript</a>
+                </li>
+            </ul>
+            Welcome, Nextjs!
+            <ul>
+                <li>
+                    <a href="/create">Create</a>
+                </li>
+                <li>
+                    <a href="/update/1">Update</a>
+                </li>
+                <li>
+                    <button>Delete</button>
+                </li>
+            </ul>
+        </div>
+    );
 }
